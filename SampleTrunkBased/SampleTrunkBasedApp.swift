d294af692aa9e0ba9b81e977ed1586345ba15046
Rangga <rangga.yudhistira@ui.ac.id>
2024-07-12 09:46:30 +0700

    Initial Commit

---
+++ SampleTrunkBased/SampleTrunkBasedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTrunkBasedApp.swift
+//  SampleTrunkBased
+//
+//  Created by Rangga Yudhistira Brata on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleTrunkBasedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
